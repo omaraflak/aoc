fff7e7fc065c52ff39d7b41a99a0667866abee8f
Omar Aflak <aflakomar@gmail.com>
2023-12-05 21:06:00 +0000

    rename var

--- 2023/05/1/main.py
+++ 2023/05/1/main.py
@@ -1,8 +1,8 @@
 Range = tuple[int, int, int]
 
 
-def map_number(n: int, map: list[Range]) -> int:
-    for dest, source, length in map:
+def map_number(n: int, maps: list[Range]) -> int:
+    for dest, source, length in maps:
         if source <= n < source + length:
             return dest + (n - source)
     return n
@@ -13,15 +13,15 @@ with open('input.txt', 'r') as file:
     seeds = [int(x) for x in lines[0].split(': ')[1].strip('\n').split(' ')]
 
     i = 3
-    map: list[Range] = list()
+    maps: list[Range] = list()
     while i < len(lines):
-        map.clear()
+        maps.clear()
         while i < len(lines) and lines[i] != '\n':
             dest, source, length = lines[i].strip('\n').split()
-            map.append((int(dest), int(source), int(length)))
+            maps.append((int(dest), int(source), int(length)))
             i += 1
 
-        seeds = [map_number(n, map) for n in seeds]
+        seeds = [map_number(n, maps) for n in seeds]
         i += 2
 
     print(min(seeds))
