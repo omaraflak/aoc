31e2284292f61a5c9995c49a9ed1eff19f7ddbdd
Omar Aflak <aflakomar@gmail.com>
2023-12-07 19:06:12 +0000

    clean up day 7

--- 2023/07/2/main.py
+++ 2023/07/2/main.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 
 order = {
-    'A': 0, 'K': 1, 'Q': 2, 'T': 4, '9': 5, '8': 6,
-    '7': 7, '6': 8, '5': 9, '4': 10, '3': 11, '2': 12, 'J': 13
+    '2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7,
+    'T': 8, 'J': -1, 'Q': 10, 'K': 11, 'A': 12,
 }
 
 
@@ -21,34 +21,35 @@ def hand_key(hand: str) -> tuple[int,...]:
         del counts['J']
     
     values = set(counts.values())
+    unique_cards = len(counts)
 
     # five of a kind
-    if len(counts) == 1:
-        hand_type = 0
+    if unique_cards == 1:
+        hand_type = 6
 
     # four of a kind
-    elif len(counts) == 2 and 4 in values:
-        hand_type = 1
+    elif unique_cards == 2 and 4 in values:
+        hand_type = 5
 
     # full house
-    elif len(counts) == 2 and 3 in values:
-        hand_type = 2
+    elif unique_cards == 2 and 3 in values:
+        hand_type = 4
 
     # three of a kind
-    elif len(counts) == 3 and 3 in values:
+    elif unique_cards == 3 and 3 in values:
         hand_type = 3
 
     # two pair
     elif sorted(counts.values()) == [1, 2, 2]:
-        hand_type = 4
+        hand_type = 2
 
     # one pair
-    elif  len(counts) == 4:
-        hand_type = 5
+    elif  unique_cards == 4:
+        hand_type = 1
 
     # high card
-    elif len(counts) == 5:
-        hand_type = 6
+    elif unique_cards == 5:
+        hand_type = 0
 
     else:
         raise RuntimeError()
@@ -60,9 +61,6 @@ def hand_key(hand: str) -> tuple[int,...]:
 with open('input.txt', 'r') as file:
     hands = [line.split() for line in file.readlines()]
     hands = [(x, int(y)) for x, y in hands]
-    result = sum(
-        hand[1] * (i + 1)
-        for i, hand in enumerate(reversed(sorted(hands, key=lambda x: hand_key(x[0]))))
-    )
+    hands.sort(key=lambda x: hand_key(x[0]))
+    result = sum(hand[1] * i for i, hand in enumerate(hands, start=1))
     print(result)
-
